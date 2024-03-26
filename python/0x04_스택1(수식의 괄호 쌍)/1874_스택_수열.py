7b3e8b83dc006760a3827437f0a4890a7de020a1
elizabethgim <yujingim@naver.com>
2024-03-26 21:35:45 +0900

    [백준, 1874] 스택 수열

--- python/0x04_스택1(수식의 괄호 쌍)/1874_스택_수열.py
+++ python/0x04_스택1(수식의 괄호 쌍)/1874_스택_수열.py
@@ -1,19 +1,29 @@
+import sys
+input = sys.stdin.readline
 n = int(input())
 stack = []
+answer = []
 i = 1
-stack.append(i)
+can = True
 
 for _ in range(n):
     nn = int(input())
 
-    if nn == i:
-        print("-")
-    else:
-        while stack[-1] < nn:
-            i += 1
-            stack.append(i)
+    while i <= nn:
+        stack.append(i)
+        answer.append('+')
+        i += 1
 
-        print("+")
+    if stack[-1] == nn:
+        stack.pop()
+        answer.append('-')
+    else:
+        print("NO")
+        can = False
+        break
+
+if can:
+    print(*answer, sep="\n")
 
 
 
